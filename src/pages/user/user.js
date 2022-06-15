194723652f1390bc329bfb097c30be60c48e0041
minhhieu <brownlaanh@gmail.com>
2022-06-15 21:38:03 +0700

    Update UI

--- src/pages/user/user.js
+++ src/pages/user/user.js
@@ -8,8 +8,7 @@ const UserManager = () => {
   const [loading, setLoading] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
 
-
-  useEffect(() => { }, []);
+  useEffect(() => {}, []);
   const columns = [
     {
       title: "Id",
@@ -103,13 +102,13 @@ const UserManager = () => {
                 <Row span={24} className="subject-filter">
                   <Col span={5} className="filter">
                     <Form.Item label="Full Name" style={{ paddingRight: 20 }}>
-                      <Input placeholder="Full Name" onChange={(e) => { }} />
+                      <Input placeholder="Full Name" onChange={(e) => {}} />
                       <Form.Item label="Tên" style={{ paddingRight: 20 }}>
-                        <Input placeholder="Tên" onChange={(e) => { }} />
+                        <Input placeholder="Tên" onChange={(e) => {}} />
                       </Form.Item>
                     </Form.Item>
                     <Form.Item label="Email">
-                      <Input placeholder="Email" onChange={(e) => { }} />
+                      <Input placeholder="Email" onChange={(e) => {}} />
                     </Form.Item>
                   </Col>
                 </Row>
