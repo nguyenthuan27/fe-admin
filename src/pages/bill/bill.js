118d0a44f2fb2ffce4161b2607d6dba0e1d06726
minhhieu <brownlaanh@gmail.com>
2022-06-15 09:06:58 +0700

    New file bill

---
+++ src/pages/bill/bill.js
@@ -0,0 +1,173 @@
+import { useEffect, useState } from "react";
+import { Col, Form, Collapse, Row, Table, Input, Button, Space, Select } from "antd";
+import { DeleteOutlined, EyeOutlined, EditTwoTone } from "@ant-design/icons";
+import "./bill.scss";
+import CreateBill from "../../component/modal/bill/create";
+import EditBill from "../../component/modal/bill/edit";
+const { Panel } = Collapse;
+const BillManager = () => {
+    const [loading, setLoading] = useState(false);
+    const [isVisibleCreate, setIsVisibleCreate] = useState(false);
+    const [isVisibleEdit, setIsVisibleEdit] = useState(false);
+    const { Option } = Select;
+    useEffect(() => { }, []);
+    const columns = [
+        {
+            title: 'Customer',
+            dataIndex: 'customer_id',
+            key: 'customer_id',
+        },
+        {
+            title: 'Voucher',
+            dataIndex: 'voucher_id',
+            key: 'voucher_id',
+        },
+        {
+            title: 'Staff',
+            dataIndex: 'staff_id',
+            key: 'staff_id',
+        },
+        {
+            title: 'Type',
+            dataIndex: 'type',
+            key: 'type',
+        },
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: 'Ship price',
+            dataIndex: 'ship_price',
+            key: 'ship_price',
+        },
+        {
+            title: 'Bill code',
+            dataIndex: 'bill_code',
+            key: 'bill_code',
+        },
+        {
+            title: 'Amount',
+            dataIndex: 'amount',
+            key: 'amount',
+        },
+        {
+            title: 'Total price',
+            dataIndex: 'total_price',
+            key: 'total_price',
+        },
+        {
+            title: 'Price after voucher',
+            dataIndex: 'price_after_voucher',
+            key: 'price_after_voucher',
+        },
+        {
+            title: 'Create date',
+            dataIndex: 'create_date',
+            key: 'create_date',
+        },
+        {
+            title: 'Update date',
+            dataIndex: 'update_date',
+            key: 'update_date',
+        },
+        {
+            title: 'State',
+            dataIndex: 'state',
+            key: 'state',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: () => (
+                <Space size="middle">
+                    <a onClick={() => setIsVisibleEdit(true)}>
+                        <EditTwoTone />Edit
+                    </a>
+                    <a>
+                        <DeleteOutlined />Delete</a>
+                </Space>
+            ),
+        },
+    ];
+    const data = [
+        {
+            key: '1',
+            customer_id: 'John',
+            lastName: 'Brown',
+            age: 32,
+            address: 'New York No. 1 Lake Park',
+            tags: ['nice', 'developer'],
+        },
+    ];
+    return (
+        <>
+            <Row className="subject-default">
+                <Col span={24} className="title">
+                    Quản lý Bill
+                </Col>
+                <Col span={24} className="subject-search">
+                    <Form layout="vertical" autoComplete="off">
+                        <Collapse>
+                            <Panel header="Tìm" key="1">
+                                <Row span={24} className="subject-filter">
+                                    <Col span={9} className="filter">
+                                        <Form.Item label="Customer" style={{ paddingRight: 20 }}>
+                                            <Input style={{ width: "100px" }} placeholder="Customer" onChange={(e) => { }} />
+                                        </Form.Item>
+                                        <Form.Item label="Staff" style={{ paddingRight: 20 }}>
+                                            <Input style={{ width: "100px" }} placeholder="Staff" onChange={(e) => { }} />
+                                        </Form.Item>
+                                        <Form.Item label="Address" style={{ paddingRight: 20 }}>
+                                            <Input style={{ width: "100px" }} placeholder="Address" onChange={(e) => { }} />
+                                        </Form.Item>
+                                    </Col>
+                                </Row>
+                            </Panel>
+                        </Collapse>
+                    </Form>
+                </Col>
+                <Col span={24} className="btn-create">
+                    <Button onClick={() => setIsVisibleCreate(true)} type="primary ">
+                        Thêm mới
+                    </Button>
+                    <Col span={24} className="sort-filter" style={{ textAlign: "right" }}>
+                        <Select
+                            defaultValue="Sort filter"
+                            style={{
+                                width: 100,
+                            }}
+                        >
+                            <Option value="10">10</Option>
+                            <Option value="15">15</Option>
+                        </Select>
+                    </Col>
+                </Col>
+
+
+                <Col span={24}>
+                    <Table
+                        dataSource={data}
+                        columns={columns}
+                        loading={loading}
+                        pagination={{
+                            pageSize: 10,
+                            showTotal: (total, range) =>
+                                `${range[0]}-${range[1]} of ${total} items`,
+                        }}
+                    />
+                </Col>
+            </Row>
+            <CreateBill
+                setIsVisible={setIsVisibleCreate}
+                isVisible={isVisibleCreate}
+            />
+            <EditBill
+                isVisible={isVisibleEdit}
+                setIsVisible={setIsVisibleEdit}
+            />
+        </>
+    );
+};
+export default BillManager;
