6f2ec2e0e26a8efd094de9e4c75590babc2837c0
minhhieu <brownlaanh@gmail.com>
2022-06-18 18:33:29 +0700

    New fide code shift and shift staff

---
+++ src/pages/shift/shift.js
@@ -0,0 +1,146 @@
+import { useEffect, useState } from "react";
+import { Col, Form, Collapse, Row, Table, Input, Button, Space, Select } from "antd";
+import { DeleteOutlined, EyeOutlined, EditTwoTone } from "@ant-design/icons";
+import "./shift.scss";
+import CreateShift from "../../component/modal/shift/create";
+import EditShift from "../../component/modal/shift/edit";
+const { Panel } = Collapse;
+const ShiftManager = () => {
+    const [loading, setLoading] = useState(false);
+    const [isVisibleCreate, setIsVisibleCreate] = useState(false);
+    const [isVisibleEdit, setIsVisibleEdit] = useState(false);
+    const { Option } = Select;
+    useEffect(() => { }, []);
+    const columns = [
+        {
+            title: 'Id',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Code',
+            dataIndex: 'code',
+            key: 'code',
+        },
+        {
+            title: 'Day',
+            dataIndex: 'day',
+            key: 'day',
+        },
+        {
+            title: 'Day name',
+            dataIndex: 'day_name',
+            key: 'day_name',
+        },
+        {
+            title: 'Start time',
+            dataIndex: 'work_start_time',
+            key: 'work_start_time',
+        },
+        {
+            title: 'End time',
+            dataIndex: 'work_start_time',
+            key: 'work_start_time',
+        },
+        {
+            title: 'Break time start',
+            dataIndex: 'break_time_start',
+            key: 'break_time_start',
+        },
+        {
+            title: 'Break time end',
+            dataIndex: 'break_time_end',
+            key: 'break_time_end',
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status',
+            key: 'status',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: () => (
+                <Space size="middle">
+                    <a onClick={() => setIsVisibleEdit(true)}>
+                        <EditTwoTone />Edit
+                    </a>
+                    <a>
+                        <DeleteOutlined />Delete</a>
+                </Space>
+            ),
+        },
+    ];
+    return (
+        <>
+            <Row className="subject-default">
+                <Col span={24} className="title">
+                    Manage Shift
+                </Col>
+                <Col span={24} className="subject-search">
+                    <Form layout="vertical" autoComplete="off">
+                        <Collapse>
+                            <Panel header="Tìm" key="1">
+                                <Row span={24} className="subject-filter">
+                                    <Col span={9} className="filter">
+                                        <Form.Item label="Name" style={{ paddingRight: 20 }}>
+                                            <Input style={{ width: "100px" }} placeholder="Name" onChange={(e) => { }} />
+                                        </Form.Item>
+                                        <Form.Item label="Code" style={{ paddingRight: 20 }}>
+                                            <Input style={{ width: "100px" }} placeholder="Code" onChange={(e) => { }} />
+                                        </Form.Item>
+
+                                    </Col>
+                                </Row>
+                            </Panel>
+                        </Collapse>
+                    </Form>
+                </Col>
+                <Col span={24} className="btn-create">
+                    <Button onClick={() => setIsVisibleCreate(true)} type="primary ">
+                        Create
+                    </Button>
+                    <Col span={24} className="sort-filter" style={{ textAlign: "right" }}>
+                        <Select
+                            defaultValue="Sort filter"
+                            style={{
+                                width: 100,
+                            }}
+                        >
+                            <Option value="10">10</Option>
+                            <Option value="15">15</Option>
+                        </Select>
+                    </Col>
+                </Col>
+
+
+                <Col span={24}>
+                    <Table
+                        dataSource={""}
+                        columns={columns}
+                        loading={loading}
+                        pagination={{
+                            pageSize: 10,
+                            showTotal: (total, range) =>
+                                `${range[0]}-${range[1]} of ${total} items`,
+                        }}
+                    />
+                </Col>
+            </Row>
+            <CreateShift
+                setIsVisible={setIsVisibleCreate}
+                isVisible={isVisibleCreate}
+            />
+            <EditShift
+                isVisible={isVisibleEdit}
+                setIsVisible={setIsVisibleEdit}
+            />
+        </>
+    );
+};
+export default ShiftManager;
