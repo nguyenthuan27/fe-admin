61b94bae92315652cd1749c43d8d4a68fbd97918
NguyenThuan <thuans2nd@gmail.com>
2022-06-24 17:18:51 +0700

    Connect Api voucher

--- src/api/manage/index.js
+++ src/api/manage/index.js
@@ -12,5 +12,55 @@ export const handleError = (err) => {
 };
 
 export default {
- 
+  getListVoucher: async () => {
+    const res = await customAxios({
+      method: "get",
+      url: `${serverEndpoint}/shoeswear/voucher/list`,
+    });
+    return res.data;
+  },
+  createVoucher: async (data) => {
+    /// Param example
+    // data = {
+    //   voucherName: "Sale 30%",
+    //   voucherCode: "30%OFF",
+    //   type: "default",
+    //   discount: 30,
+    //   pointsToReceive: "100",
+    //   maximumDiscount: 50000,
+    //   paymentType: "ALL",
+    //   minimumBill: 300000,
+    //   status: true,
+    //   note: "voucher giảm giá 30%",
+    //   releaseId: 22,
+    // };
+    const res = await customAxios({
+      method: "post",
+      data: data,
+      url: `${serverEndpoint}/shoeswear/voucher/create`,
+    });
+    return res.data;
+  },
+  updateVoucher: async (data) => {
+    const res = await customAxios({
+      method: "post",
+      url: `${serverEndpoint}/shoeswear/voucher/update`,
+      data: data
+    });
+    return res.data;
+  },
+  deleteVoucher: async (idVoucher) => {
+    const res = await customAxios({
+      method: "post",
+      url: `${serverEndpoint}/shoeswear/voucher/delete?id=${idVoucher}`
+    })
+    return res.data;
+  },
+  getListVoucherRelease: async () => {
+     const res = await customAxios({
+       method: "get",
+       url: `${serverEndpoint}/shoeswear/voucherRelease/list`,
+     });
+     return res.data;
+  }
 };
--- src/component/modal/voucher/create.js
+++ src/component/modal/voucher/create.js
@@ -1,181 +1,261 @@
 import { useEffect, useState } from "react";
-import { Button, Form, Input, InputNumber, Modal, Row, Col } from "antd";
+import {
+  Button,
+  Form,
+  Input,
+  InputNumber,
+  Modal,
+  Row,
+  Col,
+  Select,
+} from "antd";
+import API from "../../../api/manage";
+import toast, { Toaster } from "react-hot-toast";
+const { Option } = Select;
 const CreateVoucher = (props) => {
-    const { isVisible, setIsVisible } = props;
-    const layout = {
-        labelCol: { span: 20 },
-        wrapperCol: { span: 24 },
+  const { isVisible, setIsVisible, vouchers, getListVoucher } = props;
+  const [listRelease, setListRelease] = useState([]);
+  const [form] = Form.useForm();
+  const layout = {
+    labelCol: { span: 20 },
+    wrapperCol: { span: 24 },
+  };
+  const onFinish = (value) => {
+    let data = {
+      id: isVisible.id || "",
+      voucherName: value.voucherName,
+      voucherCode: value.voucherCode,
+      type: value.type,
+      discount: value.discount,
+      pointsToReceive: value.pointsToReceive,
+      maximumDiscount: value.maximumDiscount,
+      paymentType: value.paymentType,
+      minimumBill: value.minimumBill,
+      status: true,
+      note: value.note,
+      releaseId: value.releaseId,
     };
-    const onFinish = () => {
-
+    if (isVisible.action == "create") {
+      createVoucher(data);
+    } else {
+      editVoucher(data);
     }
-    const validateMessages = {
-        required: "${label} is required!",
-        types: {
-            email: "${label} is not a valid email!",
-            number: "${label} is not a valid number!",
-        },
-        number: {
-            range: "${label} must be between ${min} and ${max}",
-        },
-    };
-    useEffect(() => { }, []);
-    return (
-        <>
-            <Modal
-                title="Thêm voucher"
-                centered
-                visible={isVisible}
-                onCancel={() => setIsVisible(false)}
-                footer={null}
-            >
-                <Form
-                    {...layout}
-                    name="nest-messages"
-                    onFinish={onFinish}
-                    layout="vertical"
-                    validateMessages={validateMessages}
-                >
-                    <Row gutter={{
-                        xs: 8,
-                        sm: 16,
-                        md: 24,
-                        lg: 59
-                    }} justify="left">
-                        <Col>
-                            <Form.Item
-                                name={["bill", "id"]}
-                                label="Id"
-                                rules={[{ required: true, type: "number" }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "release_id"]}
-                                label="Release"
-                                rules={[{ required: true }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
+  };
+  const validateMessages = {
+    required: "${label} is required!",
+    types: {
+      email: "${label} is not a valid email!",
+      number: "${label} is not a valid number!",
+    },
+    number: {
+      range: "${label} must be between ${min} and ${max}",
+    },
+  };
 
-                        <Col>
-                            <Form.Item
-                                name={["bill", "voucher_name"]}
-                                label="Voucher name"
-                                rules={[{ required: true }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "voucher_code"]}
-                                label="Voucher code"
-                                rules={[{ required: true }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "note"]}
-                                label="Note"
-                                rules={[{ required: true }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "discount"]}
-                                label="Discount"
-                                rules={[{ required: true, type: "number" }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "type"]}
-                                label="Type"
-                                rules={[{ required: true }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "points_to_receive"]}
-                                label="Points to receive"
-                                rules={[{ required: true }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "maximum_discount"]}
-                                label="Maximum discount"
-                                rules={[{ required: true, type: "number" }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "payment_type"]}
-                                label="Payment type"
-                                rules={[{ required: true, type: "number" }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "minimum_bill"]}
-                                label="Minimum bill"
-                                rules={[{ required: true, type: "number" }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "create_date"]}
-                                label="Create date"
-                                rules={[{ required: true }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col>
-                            <Form.Item
-                                name={["bill", "status"]}
-                                label="Status"
-                                rules={[{ required: true }]}
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                    </Row>
-                    <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 9 }}>
-                        <Button type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                        <Button
-                            type="primary"
-                            onClick={() => setIsVisible(false)}
-                            style={{ marginLeft: "5px" }}
-                        >
-                            Cancel
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </Modal>
-        </>
-    );
+  const createVoucher = async (data) => {
+    let create = await API.createVoucher(data);
+    if (create.message === "SUCCESS") {
+      toast.success(create.message);
+      getListVoucher();
+      setIsVisible({ type: false, action: isVisible.action });
+    } else {
+      toast.error(create.message);
+    }
+  };
+
+  const editVoucher = async (data) => {
+    let update = await API.updateVoucher(data);
+    if (update.message === "SUCCESS") {
+      toast.success(update.message);
+      getListVoucher();
+      setIsVisible({ type: false, action: isVisible.action });
+    } else {
+      toast.error(update.message);
+    }
+  };
+
+  const getListVoucherRelease = async () => {
+    let res = await API.getListVoucherRelease();
+    const listId = res.result.map((item) => item.id);
+    setListRelease(listId);
+  };
+  useEffect(() => {
+    getListVoucherRelease();
+    let listDataVoucher = vouchers || [];
+    const voucher =
+      listDataVoucher?.find((item) => item.id == isVisible?.id) || [];
+    form.setFieldsValue({
+      id: voucher.id,
+      voucherName: voucher.voucherName,
+      voucherCode: voucher.voucherCode,
+      type: voucher.type,
+      discount: voucher.discount,
+      pointsToReceive: voucher.pointsToReceive,
+      maximumDiscount: voucher.maximumDiscount,
+      paymentType: voucher.paymentType,
+      minimumBill: voucher.minimumBill,
+      createDate: voucher.createDate,
+      status: voucher.status,
+      note: voucher.note,
+      releaseId: voucher.releaseId,
+    });
+  }, [isVisible.type]);
+  return (
+    <>
+      <Toaster toastOptions={{ position: "top-center" }} />
+      <Modal
+        title="Thêm voucher"
+        centered
+        visible={isVisible.type}
+        onCancel={() => setIsVisible({ type: false, action: isVisible.action })}
+        footer={null}
+      >
+        <Form
+          {...layout}
+          form={form}
+          name="nest-messages"
+          onFinish={onFinish}
+          layout="vertical"
+          validateMessages={validateMessages}
+        >
+          <Row
+            gutter={{
+              xs: 8,
+              sm: 16,
+              md: 24,
+              lg: 59,
+            }}
+            justify="left"
+          >
+            <Col>
+              <Form.Item name="id" label="Id">
+                <Input disabled />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item
+                name="releaseId"
+                label="Release Id"
+                rules={[{ required: true }]}
+              >
+                <Select
+                  placeholder="Please select"
+                  style={{
+                    width: "calc(100%)",
+                  }}
+                >
+                  {listRelease.map((data, key) => {
+                    return (
+                      <Option key={key} value={data}>
+                        {data}
+                      </Option>
+                    );
+                  })}
+                </Select>
+              </Form.Item>
+            </Col>
+
+            <Col>
+              <Form.Item
+                name="voucherName"
+                label="Voucher name"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item
+                name="voucherCode"
+                label="Voucher code"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item name="note" label="Note" rules={[{ required: true }]}>
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item
+                name="discount"
+                label="Discount"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item name="type" label="Type" rules={[{ required: true }]}>
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item
+                name="pointsToReceive"
+                label="Points to receive"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item
+                name="maximumDiscount"
+                label="Maximum discount"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item
+                name="paymentType"
+                label="Payment type"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item
+                name="minimumBill"
+                label="Minimum bill"
+                rules={[{ required: true }]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 9 }}>
+            {isVisible.action == "create" ? (
+              <>
+                <Button type="primary" htmlType="submit">
+                  Create
+                </Button>
+              </>
+            ) : (
+              <>
+                <Button type="primary" htmlType="submit">
+                  Edit
+                </Button>
+              </>
+            )}
+            <Button
+              type="primary"
+              onClick={() =>
+                setIsVisible({ type: false, action: isVisible.action })
+              }
+              style={{ marginLeft: "5px" }}
+            >
+              Cancel
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
+  );
 };
 export default CreateVoucher;
