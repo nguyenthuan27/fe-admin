f76baea6eb8ee8b8ad2620396b1a84585aabf4f2
Jack <jack@dstarter.io>
2022-07-01 22:18:07 +0700

    Update

--- src/component/modal/billInfo.js
+++ src/component/modal/billInfo.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import { Button, Form, Input, InputNumber, Modal, Space, Row, Col } from "antd";
 const BillModal = (props) => {
-  const { isVisible, setIsVisible } = props;
+  const { isVisible, setIsVisible, listBill, getListBill } = props;
+  const [form] = Form.useForm();
   const layout = {
     labelCol: { span: 20 },
     wrapperCol: { span: 24 },
   };
-  const onFinish = () => { };
+  const onFinish = (value) => {
+    console.log(value);
+  };
   const validateMessages = {
     required: "${label} is required!",
     types: {
@@ -17,14 +20,30 @@ const BillModal = (props) => {
       range: "${label} must be between ${min} and ${max}",
     },
   };
-  useEffect(() => { }, []);
+  useEffect(() => {
+    let listData = listBill || [];
+    const bills = listData?.find((item) => item.bill_id == isVisible?.id) || [];
+    form.setFieldsValue({
+      // address: bills.address,
+      // amount: bills.amount,
+      // bill_code: bills.bill_code,
+      // customer_id: bills.customer?.customer_name,
+      // ship_price: "ytry",
+      // staff_id: "ytry",
+      // state: "ytry",
+      // total_price: bills.total_price,
+      // type: "ytryt",
+      // update_date: "ytry",
+      // voucher_id: "ytry",
+    });
+  }, [isVisible.type]);
   return (
     <>
       <Modal
         title="Thêm Bill"
         centered
-        visible={isVisible}
-        onCancel={() => setIsVisible(false)}
+        visible={isVisible.type}
+        onCancel={() => setIsVisible({ type: false, action: isVisible.action })}
         footer={null}
       >
         <Form
@@ -34,12 +53,15 @@ const BillModal = (props) => {
           layout="vertical"
           validateMessages={validateMessages}
         >
-          <Row gutter={{
-            xs: 8,
-            sm: 16,
-            md: 24,
-            lg: 59
-          }} justify="left">
+          <Row
+            gutter={{
+              xs: 8,
+              sm: 16,
+              md: 24,
+              lg: 59,
+            }}
+            justify="left"
+          >
             <Col>
               <Form.Item
                 name={["bill", "customer_id"]}
@@ -91,7 +113,7 @@ const BillModal = (props) => {
               <Form.Item
                 name={["bill", "ship_price"]}
                 label="Ship price"
-                rules={[{ required: true, type: "number" }]}
+                rules={[{ required: true }]}
               >
                 <Input />
               </Form.Item>
@@ -120,7 +142,7 @@ const BillModal = (props) => {
               <Form.Item
                 name={["bill", "total_price"]}
                 label="Total price"
-                rules={[{ required: true, type: "number" }]}
+                rules={[{ required: true }]}
               >
                 <Input />
               </Form.Item>
@@ -129,7 +151,7 @@ const BillModal = (props) => {
               <Form.Item
                 name={["bill", "price_after_voucher"]}
                 label="Price after voucher"
-                rules={[{ required: true, type: "number" }]}
+                rules={[{ required: true }]}
               >
                 <Input />
               </Form.Item>
@@ -178,7 +200,7 @@ const BillModal = (props) => {
             </Button>
           </Form.Item>
         </Form>
-      </Modal >
+      </Modal>
     </>
   );
 };
